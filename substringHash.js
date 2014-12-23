e3d6444b113314d082ae61ef464b7319a0e5b1eb
vikas17a <vikas17a@gmail>
2014-12-24 00:03:45 +0530

    improved and remove buffer

--- substringHash.js
+++ substringHash.js
@@ -7,13 +7,14 @@ var hash = function newHash(arrayOfString){
 	* Parameters : string , position of character to update hashValue,value( previous hash value)
 	*/
 	hash = function(string, pos, value){
-		var p = value, d=10;
+		var p = value, d=0;
 		p = (p*d + string.charCodeAt(pos))%997;
 		return p;
 	};
 	
 	//Hash Value Container
-	var hashValue = new Array();
+	//var hashValue = new Array();
+	var maps = {};
 	
 	//Hash Value of String to be entered by User for search
 	var hashPat = 0;
@@ -23,15 +24,30 @@ var hash = function newHash(arrayOfString){
 	*/
 	var initialize = function intialize(){
 		for(var i = 0 ; i < arrayOfString.length ; i++){
-			hashValue[i] = [];
-			hashValue[i][0] = hash(arrayOfString[i], 0, 0);
-			hashValue[i][1] = i;	
+			var p = hash(arrayOfString[i], 0, 0);
+			if(p in maps){
+				maps[p].push(i);
+			}
+			else{
+				maps[p] = new Array();
+				maps[p].push(i);
+			}
 		}
+		//for(x in maps)
+		//	console.log(maps[x]);
 	}
 	
 	/*Call to initialize
 	*/
 	initialize();
+	
+	/* Remove hash entries function
+	*/
+	var emptyHash = function emptyHash(){
+		for(x in maps)
+			delete maps[x];
+	};
+
 	
 	/*Search Function to match the hashPat and HashValue and also doing pruning to remove obsolete strings
 	  Parameter string to be searched entered by user character by character		
@@ -39,42 +55,68 @@ var hash = function newHash(arrayOfString){
 	searchHash = function(string){
 		var len = string.length, j = 0;
 		if(len == 0){
+			emptyHash();
 			hashPat = 0;
-			hashValue = [];
 			initialize();
 			return "";
 		}
 		hashPat = hash(string, len-1, hashPat);
-		var prunedHash = new Array(), result = new Array();
+		var result = new Array();
 		result[j] = "";
-		for(var i = 0; i < hashValue.length; i++){
-			if(hashValue[i][0] == hashPat){
-				prunedHash[j] = [];
-				prunedHash[j][0] = hashValue[i][0];
-				prunedHash[j][1] = hashValue[i][1];
-				result[j]  = arrayOfString[hashValue[i][1]];
+		if(hashPat in maps){
+			for(var i = 0; i < maps[hashPat].length; i++){
+				result[j] = arrayOfString[maps[hashPat][i]];
 				j++;
 			}
 		}
-		hashValue = [];
-		hashValue = prunedHash.slice(0);
-		for(var i = 0; i < hashValue.length; i++){
-			hashValue[i][0] = hash(arrayOfString[hashValue[i][1]],len,hashValue[i][0]);
+		else{
+			emptyHash();
+			return result;
+		}
+		var requiredKeys = new Array();
+		requiredKeys = maps[hashPat].slice(0);
+		//console.log(requiredKeys);
+		emptyHash();
+		for(var i = 0; i < requiredKeys.length; i++){
+			var p = hash(arrayOfString[requiredKeys[i]], len, hashPat);
+			if(p in maps){
+				maps[p].push(requiredKeys[i]);
+			}
+			else{
+				maps[p] = new Array();
+				maps[p].push(requiredKeys[i]);
+			}
 		}
 		return result;				
 	};
 	return{
+		searchx : function(string){
+				return searchHash(string);
+		},
 		search : function(string){
-			return searchHash(string);
+				searchHash('');
+				var len = string.length;
+				var k = 0;
+				for(k = 0; k < len; k++){
+					if(k == (len-1)){
+						return searchHash(string.substr(0,k+1));						
+					}
+					else{
+						searchHash(string.substr(0,k+1));
+					}
+				}
 		}
 	};
-}
+};
 
 var arr = new Array();
 arr = ["hello", "smile", "how", "are", "you", "blue", "green", "red", "hiver", "river"];
 var subHash = new hash(arr);
-console.log("h : " + subHash.search('h'));
-console.log("he : " + subHash.search('he'));
-console.log(" : " + subHash.search(''));
-console.log("b : " + subHash.search('b'));
-console.log("bl : " + subHash.search('bl'));
+console.log(subHash.search('hello'));
+console.log(subHash.search('b'));
+console.log(subHash.search('bl'));
+console.log(subHash.search('r'));
+console.log(subHash.search("ve"));
+console.log(subHash.search("ver"));
+console.log(subHash.search('hell'));
+console.log(subHash.search('ver'));
